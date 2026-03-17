77bc8bf62a92f18a28db5605a78779c4555f2491
WAN719 <3425446714@qq.com>
2026-03-17 20:21:10 +0800

    first commit

---
+++ src/main/java/cn/edu/swu/campus/CampusApplication.java
@@ -0,0 +1,14 @@
+package cn.edu.swu.campus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@EnableAutoConfiguration
+@ComponentScan
+@Configuration
+@SpringBootApplication
+public class CampusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampusApplication.class, args);
+	}
+}
