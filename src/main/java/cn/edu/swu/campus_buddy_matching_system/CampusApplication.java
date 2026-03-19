832de91a4c4a229d320a52e4c8f041bfe738c286
WAN719 <3425446714@qq.com>
2026-03-19 10:56:29 +0800

    添加基础配置

--- src/main/java/cn/edu/swu/campus_buddy_matching_system/CampusApplication.java
+++ src/main/java/cn/edu/swu/campus_buddy_matching_system/CampusApplication.java
@@ -1,7 +1,10 @@
-package cn.edu.swu.campus;
+package cn.edu.swu.campus_buddy_matching_system;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 @EnableAutoConfiguration
 @ComponentScan
 @Configuration
